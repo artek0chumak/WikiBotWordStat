35041f5bc8402ac7e8950b78eed1b1cb1bc3412e
artek.chumak <artek.chumak@gmail.com>
2018-05-07 23:29:55 +0300

    Написание парсера и скелета бота.

---
+++ bot.py
@@ -0,0 +1,45 @@
+"""
+Файл для работы с Telegram ботом
+"""
+import telebot
+
+
+with open('config') as config:
+    bot = telebot.TeleBot(config['token'])
+
+@bot.message_handler(regexp='help')
+def show_help(message):
+    pass
+
+@bot.message_handler(regexp='write [\d]+')
+def write_topN(message):
+    list_m = message.split()
+    N = int(list_m[1])
+    pass
+
+@bot.message_handler(regexp='top [\d]+ (asc|desc)')
+def show_topN(message):
+    list_m = message.split()
+    N = int(list_m[1])
+    is_asc = list_m[2].lower_case() == 'asc'
+    pass
+
+@bot.message_handler(regexp='stop word')
+def stop_word(message):
+    pass
+
+@bot.message_handler(regexp='word cloud [\w]+')
+def word_cloud(message):
+    list_m = message.split()
+    color = list_m[-1]
+    pass
+
+@bot.message_handler(regexp='describe')
+def describe(message):
+    pass
+
+@bot.message_handler(regexp='describe [\w]+')
+def describe_word(message):
+    list_m = message.split()
+    word = list_m[-1]
+    pass
---
+++ parser.py
@@ -0,0 +1,40 @@
+"""
+Файл для парсинга сайта
+"""
+import requests
+from lxml import etree
+from io import StringIO
+
+
+class UrlParser:
+    """
+    Парсер сайта, возвращает текст из <p> и ссылки из сайта.
+    """
+    def __init__(self, url):
+        """
+        :param url: Ссылка на сайт
+        :rtype: str
+        """
+        responce = requests.get(url)
+        html = responce.text
+        parser = etree.HTMLParser()
+
+        self.tree = etree.parse(StringIO(html), parser)
+
+    def find_paragraphs(self):
+        """
+        Поиск всех данных из сайта с тэгом <p></p>
+        :return: Генератор данных
+        :rtype: generator
+        """
+        for p in self.tree.xpath('//p'):
+            yield p
+
+    def find_references(self):
+        """
+        Поиск ссылок в сайте
+        :return: Генератор данных
+        :rtype: generator
+        """
+        for href in self.tree.xpath('//href'):
+            yield href.attrib.get('href', None)
